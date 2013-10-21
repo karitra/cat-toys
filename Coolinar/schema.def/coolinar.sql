f2c8046be0da56b94f9084e2469fb7fca7e7f0d0
karitra <karev.alexandre@gmail.com>
2013-10-21 10:56:04 +0400

    code formating update

--- Coolinar/schema.def/coolinar.sql
+++ Coolinar/schema.def/coolinar.sql
@@ -7,60 +7,60 @@ PRAGMA foreign_keys = ON;
 --     - ingredients
 --     - users who creates it
 --
-create table recipes (
-       id integer primary key autoincrement,
-       name varchar(128) not null,
-       desc text not null,
-       img blob,
-       thumbnail blob,
-       when_created timestamp not null default current_timestamp,
-       when_updated timestamp not null default current_timestamp
+CREATE TABLE recipes (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       name VARCHAR(128) NOT NULL,
+       desc TEXT NOT NULL,
+       img BLOB,
+       thumbnail BLOB,
+       when_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       when_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-create table roles (
-       id integer primary key autoincrement,
-       role varchar(32) not null,
-       desc text
+CREATE TABLE roles (
+       id iNTEGER PRIMARY KEY AUTOINCREMENT,
+       role VARCHAR(32) NOT NULL,
+       desc TEXT
 );
 
-create table users (
-       id integer primary key autoincrement,
-       name varchar(32),
+CREATE TABLE users (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       name VARCHAR(32),
        -- left out: address/user info, user stat
-       pass varchar(32),
-       r_id int references roles(id),
-       when_created timestamp not null default current_timestamp,
-       when_updated timestamp not null default current_timestamp
+       pass VARCHAR(32),
+       r_id INT REFERENCES ROLES(id),
+       when_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       when_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
-create table comments (
-       id integer primary key  autoincrement,
-       u_id int references users(id),
-       r_id int references recipes(id),
-       rate int,
-       post text,
-       when_posted timestamp not null default current_timestamp
+CREATE TABLE comments (
+       id INTEGER PRIMARY KEY  AUTOINCREMENT,
+       u_id INT REFERENCES USERS(id),
+       r_id INT REFERENCES RECIPES(id),
+       rate INT,
+       post TEXT,
+       when_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 --
 -- TODO: multiple recipe authors support
 --
-create table recipe_author (
-       r_id int references recipes(id),
-       u_id int references users(id)
+CREATE TABLE recipe_author (
+       r_id INT REFERENCES RECIPES(id),
+       u_id INT REFERENCES USERS(id)
 );
 
-insert into roles(id, role) values(1, 'guest');
-insert into roles(id, role) values(2, 'user' );
-insert into roles(id, role) values(3, 'admin');
+INSERT INTO roles(id, role) VALUES(1, 'guest');
+INSERT INTO roles(id, role) VALUES(2, 'user' );
+INSERT INTO roles(id, role) VALUES(3, 'admin');
 
-insert into users(name, pass, r_id) values( 'guest',  'guest',  2 );
-insert into users(name, pass, r_id) values( 'guest1', 'guest1', 2 );
+INSERT INTO users(name, pass, r_id) VALUES( 'guest',  'guest',  2 );
+INSERT INTO users(name, pass, r_id) VALUES( 'guest1', 'guest1', 2 );
 
-insert into recipes(name, desc) values( 'Бутерброд (с белым хлебом)', 'Берём буханку, отрезаем ломтик (не менее 3 см в ширину), намазываем масло, кладём сверху заготовленную котлету (см рецепт по приготовлению котлет по киевски)');
-insert into recipes(name, desc) values( 'Чай', 'Греем воду (не менее 300 мл) до состояния кипения, устанавливаем покетик с чаем в кружку, таким образом чтобы нить с захватом оставалась вне кружки, наливаем в кружку подогретую воду. Не долеваем до бортика (края) кружки несколько сантиметорв. По желанию добовляем сахар, но не более двух ложек. Подождать несколько минут, пока температура воды понизится, а чай зварится.');
-insert into recipes(name, desc) values( 'Хот-Дог', 'Очень долго рассказывать, но варим несколко минут сосиску, затем берём булку, разрезаем на две части (вдоль), кладём в неё упомянутую сосиску, льём сверху кетчуп');
+INSERT INTO recipes(name, desc) VALUES( 'Бутерброд (с белым хлебом)', 'Берём буханку, отрезаем ломтик (не менее 3 см в ширину), намазываем масло, кладём сверху заготовленную котлету (см рецепт по приготовлению котлет по киевски)');
+INSERT INTO recipes(name, desc) VALUES( 'Чай', 'Греем воду (не менее 300 мл) до состояния кипения, устанавливаем покетик с чаем в кружку, таким образом чтобы нить с захватом оставалась вне кружки, наливаем в кружку подогретую воду. Не долеваем до бортика (края) кружки несколько сантиметорв. По желанию добовляем сахар, но не более двух ложек. Подождать несколько минут, пока температура воды понизится, а чай зварится.');
+INSERT INTO recipes(name, desc) VALUES( 'Хот-Дог', 'Очень долго рассказывать, но варим несколко минут сосиску, затем берём булку, разрезаем на две части (вдоль), кладём в неё упомянутую сосиску, льём сверху кетчуп');
 
 --
 -- Local Variables:
